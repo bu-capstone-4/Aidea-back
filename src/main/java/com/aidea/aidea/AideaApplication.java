c9426388469e9bd34801d0d412bdc4187ed4c179
BH <kbh02180@gamil.com>
2026-04-16 10:20:57 +0900

    초기 파일 설정

---
+++ src/main/java/com/aidea/aidea/AideaApplication.java
@@ -0,0 +1,13 @@
+package com.aidea.aidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AideaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aidea/aidea/AideaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidea.aidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
