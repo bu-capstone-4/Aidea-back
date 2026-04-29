d7e44cf3b3ab91e3f3f4f2f0d32908169ba82628
BH <kbh02180@gmail.com>
2026-04-30 02:19:28 +0900

    문서 엔티티 수정

--- src/main/java/com/aidea/aidea/domain/documents/entity/Document.java
+++ src/main/java/com/aidea/aidea/domain/documents/entity/Document.java
@@ -1,19 +1,39 @@
 package com.aidea.aidea.domain.documents.entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
-@Data
+@Table(name = "document")
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Document {
 
     @Id
-    private String docId;
+    @Column(name = "id")
+    private String id;
 
+    @Column(name = "teamspace_id", nullable = false)
+    private String teamspaceId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private DocumentType type;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "yjs_binary", nullable = false)
+    private byte[] yjsBinary;
+
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
 }
