d1d4a2d62760da6a9415f7a83db7267e51186331
BH <kbh02180@gmail.com>
2026-05-07 02:37:57 +0900

    공통 응답 처리

--- src/main/java/com/aidea/aidea/domain/teamspace/controller/TeamSpaceController.java
+++ src/main/java/com/aidea/aidea/domain/teamspace/controller/TeamSpaceController.java
@@ -2,6 +2,7 @@ package com.aidea.aidea.domain.teamspace.controller;
 
 import com.aidea.aidea.domain.teamspace.dto.*;
 import com.aidea.aidea.domain.teamspace.service.TeamSpaceService;
+import com.aidea.aidea.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,29 +13,35 @@ public class TeamSpaceController {
 
     private final TeamSpaceService teamSpaceService;
 
+    // 팀스페이스 생성
     @PostMapping
-    public TeamSpaceCreateResponse create(@RequestBody TeamSpaceCreateRequest request) {
-        return teamSpaceService.create(request);
+    public ApiResponse<TeamSpaceCreateResponse> create(@RequestBody TeamSpaceCreateRequest request) {
+        return ApiResponse.ok(teamSpaceService.create(request));
     }
 
-    @GetMapping("/{id}")
-    public TeamSpaceDetailResponse get(@PathVariable String id) {
-        return teamSpaceService.get(id);
+    // 팀스페이스 단건 조회
+    @GetMapping("/{teamspaceId}")
+    public ApiResponse<TeamSpaceDetailResponse> get(@PathVariable String teamspaceId) {
+        return ApiResponse.ok(teamSpaceService.get(teamspaceId));
     }
 
+    // 팀스페이스 목록 조회
     @GetMapping
-    public TeamSpaceListResponse getList() {
-        return teamSpaceService.getList();
+    public ApiResponse<TeamSpaceListResponse> getList() {
+        return ApiResponse.ok(teamSpaceService.getList());
     }
 
-    @PutMapping("/{id}")
-    public TeamSpaceCreateResponse update(@PathVariable String id,
-                                          @RequestBody TeamSpaceUpdateRequest request) {
-        return teamSpaceService.update(id, request);
+    // 팀스페이스 수정(이름)
+    @PutMapping("/{teamspaceId}")
+    public ApiResponse<TeamSpaceCreateResponse> update(@PathVariable String teamspaceId,
+                                                       @RequestBody TeamSpaceUpdateRequest request) {
+        return ApiResponse.ok(teamSpaceService.update(teamspaceId, request));
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id) {
-        teamSpaceService.delete(id);
+    // 팀스페이스 삭제
+    @DeleteMapping("/{teamspaceId}")
+    public ApiResponse<Void> delete(@PathVariable String teamspaceId) {
+        teamSpaceService.delete(teamspaceId);
+        return ApiResponse.ok();
     }
 }
