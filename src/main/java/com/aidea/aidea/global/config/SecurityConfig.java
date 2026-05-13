7d7e4a8173a7c8372069560b64a08f16b233e20c
BH <kbh02180@gmail.com>
2026-05-14 02:36:47 +0900

    초대 관련 url 추가

--- src/main/java/com/aidea/aidea/global/config/SecurityConfig.java
+++ src/main/java/com/aidea/aidea/global/config/SecurityConfig.java
@@ -56,7 +56,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/actuator/health"
+                                "/actuator/health",
+                                "/invitations/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
