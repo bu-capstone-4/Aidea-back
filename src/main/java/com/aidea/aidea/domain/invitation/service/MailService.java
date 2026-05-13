8b1440f728ccd72119f0670a4193780b160ebcef
BH <kbh02180@gmail.com>
2026-05-14 02:36:55 +0900

    메일 서비스 생성

---
+++ src/main/java/com/aidea/aidea/domain/invitation/service/MailService.java
@@ -0,0 +1,22 @@
+package com.aidea.aidea.domain.invitation.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendInvitationMail(String to, String inviteLink) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("[AIdea] 초대가 도착했습니다");
+        message.setText("아래 링크를 클릭해 초대를 수락하세요.\n\n" + inviteLink + "\n\n링크는 48시간 후 만료됩니다.");
+        mailSender.send(message);
+    }
+}
